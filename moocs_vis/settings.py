39956d43662cc9fef155d433e018dcc2f54d99db
Duy Nguyen <duy.phnguyen@gmail.com>
2014-05-09 07:59:34 +0200

    reconfig database

--- moocs_vis/settings.py
+++ moocs_vis/settings.py
@@ -28,20 +28,20 @@ BASE_DIR = os.path.dirname(__file__)
 # }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'moocs_vis_test',
-        'USER': 'root',
-        'HOST': 'localhost',
-    }
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': os.environ['RDS_DB_NAME'],
-    #     'USER': os.environ['RDS_USERNAME'],
-    #     'PASSWORD': os.environ['RDS_PASSWORD'],
-    #     'HOST': os.environ['RDS_HOSTNAME'],
-    #     'PORT': os.environ['RDS_PORT'],
+    #     'NAME': 'moocs_vis_test',
+    #     'USER': 'root',
+    #     'HOST': 'localhost',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
+    }
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
