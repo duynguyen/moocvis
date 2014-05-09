4619cf951b2d4b24742847bc963e753a04ba8f68
Duy Nguyen <duy.phnguyen@gmail.com>
2014-05-09 15:13:37 +0200

    modify createadmin script

--- scripts/createadmin.py
+++ scripts/createadmin.py
@@ -4,12 +4,6 @@ from django.contrib.auth.models import User
 from lectures.models import *
 from lectures.clickstream_handler import importLectures, importUsers, importSlides, importBehaviors, getArrayFromCsv
 
-if User.objects.count() == 0:
-	admin = User.objects.create(username='admin')
-	admin.set_password('123456')
-	admin.is_superuser = True
-	admin.is_staff = True
-	admin.save()
 
 Behavior.objects.all().delete()
 User.objects.all().delete()
@@ -17,6 +11,13 @@ SlidePlay.objects.all().delete()
 Slide.objects.all().delete()
 Lecture.objects.all().delete()
 Course.objects.all().delete()
+
+if User.objects.count() == 0:
+	admin = User.objects.create(username='admin')
+	admin.set_password('123456')
+	admin.is_superuser = True
+	admin.is_staff = True
+	admin.save()
 
 Course(name="Functional Programming Principles in Scala").save()
 importLectures()
