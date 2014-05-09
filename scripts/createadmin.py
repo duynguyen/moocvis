47e2f0dbec4eb03c59bf471546aa431d833a8136
Duy Nguyen <duy.phnguyen@gmail.com>
2014-05-09 13:56:37 +0200

    automate script of populating data

--- scripts/createadmin.py
+++ scripts/createadmin.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 from django.contrib.auth.models import User
-from lectures.models import Course
-from lectures.clickstream_handler import importLectures, importUsers
+from lectures.models import *
+from lectures.clickstream_handler import importLectures, importUsers, importSlides, importBehaviors, getArrayFromCsv
 
 if User.objects.count() == 0:
 	admin = User.objects.create(username='admin')
@@ -11,6 +11,17 @@ if User.objects.count() == 0:
 	admin.is_staff = True
 	admin.save()
 
+Behavior.objects.all().delete()
+User.objects.all().delete()
+SlidePlay.objects.all().delete()
+Slide.objects.all().delete()
+Lecture.objects.all().delete()
+Course.objects.all().delete()
+
 Course(name="Functional Programming Principles in Scala").save()
 importLectures()
 importUsers()
+importSlides(getArrayFromCsv('6-5_slides.csv'), 6, 5)
+importBehaviors(getArrayFromCsv('6-5.csv'), 6, 5)
+importSlides(getArrayFromCsv('6-2_slides.csv'), 6, 2)
+importBehaviors(getArrayFromCsv('6-2.csv'), 6, 2)
