9c4f6ef1b81adcc84e4757722da9981307677ed8
Duy Nguyen <duy.phnguyen@gmail.com>
2014-07-21 09:55:45 +0200

    remove local env

--- moocs_vis_env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/duynguyen/code/cede_project/clickstream-analyzer/src/visualization/moocs_vis/moocs_vis_env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
